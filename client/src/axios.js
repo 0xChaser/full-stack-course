4a4b6eaf7510373cab24e59dee23381895bb35ea
chaser <florian.isak@icloud.com>
2025-09-15 13:44:37 +0200

    Add files for deploy

--- client/src/axios.js
+++ client/src/axios.js
@@ -2,7 +2,7 @@ import axios from 'axios'
 
 
 export const apiClient = axios.create({
-    baseURL: "https://full-js.flo-isk.fr",
+    baseURL: "https://full-js-api.flo-isk.fr",
     headers: {
         'Content-Type': 'application/json',
       },
